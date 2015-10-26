9c3b508f692dc7b24aa38de6d3493a9ff9982293
Julian Ceballos <cristianjulianceballos@gmail.com>
2015-10-26 17:04:14 -0600

    add Conekta devicecollect

--- Conekta-Xamarin/DeviceCollector.cs
+++ Conekta-Xamarin/DeviceCollector.cs
@@ -15,29 +15,6 @@ namespace ConektaXamarin
 
 	public class DeviceCollector {
 
-		#if __IOS__
-		public UIViewController _delegate { get; set; }
-		#endif
-		public string PublicKey;
 
-		public static void Collect(String SessionId)
-		{
-			string html = "<html style=\"background: blue;\"><head></head><body>";
-			html += "<script type=\"text/javascript\" src=\"https://conektaapi.s3.amazonaws.com/v0.3.2/js/conekta.js\" data-public-key=\"" + this.PublicKey + "\" data-session-id=\"" + SessionId + "\"></script>";
-			html += "</body></html>";
-
-			string contentPath = Environment.CurrentDirectory;
-
-			#if __IOS__
-			UIWebView web = new UIWebView(new RectangleF(new PointF(0,0), new SizeF(0, 0)));
-			web.LoadHtmlString(html, new NSUrl(contentPath, true));
-			web.ScalesPageToFit = true;
-			this._delegate.View.AddSubview(web);
-			#endif
-
-			#if __ANDROID__
-
-			#endif
-		}
 	}
 }
